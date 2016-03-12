206520f3160116aa8a23ccf5764fbfb5ec465d3a
Marcus Gustafsson <margu478@student.liu.se>
2016-03-12 10:13:32 +0100

    removed use of localhost

--- static/index.js
+++ static/index.js
@@ -5,7 +5,7 @@ accessApp.controller("accessFormController", function($scope, $http) {
     $scope.submitAccessForm = function() {
         $http({
             method  : 'POST',
-            url     : 'http://localhost:3000/api/access',
+            url     : 'http://fishdrop.xyz:3000/api/access',
             data    : $scope.access,
             headers : {'Content-Type': 'application/json'}
         }).success(function(data) {
